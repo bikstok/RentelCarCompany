d4b68f09dc13ca6b6674d28e83016b81c11b2e0a
MadsLindersson <Mali0002@stud.kea.dk>
2024-04-23 11:55:47 +0200

    Added fiels and constructers to all vehicle classes.

--- src/Family.java
+++ src/Family.java
@@ -1,6 +1,15 @@
-public class Family {
-    int hej;
-    String fuckEtLorteProgram;
-    int i;
-    int j;
+public class Family extends Vehicle {
+
+    int trunkLiters;
+    public Family (String brand, String model, String fuelType, int registrationNumber,
+                   int firstRegistrationYearAndMonth, int odometer, boolean gear, String seats,
+                   boolean aircon, boolean cruiseControl, int horsePower, int trunkLiters)    {
+
+        super(brand, model, fuelType, registrationNumber,
+        firstRegistrationYearAndMonth, odometer, gear, seats,
+        aircon, cruiseControl, horsePower);
+
+        this.trunkLiters = trunkLiters;
+
+    }
 }
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,2 +1,31 @@
 public class Vehicle {
+    String brand;
+    String model;
+    String fuelType;
+    int registrationNumber;
+    int firstRegistrationYearAndMonth;
+    int odometer;
+    boolean gear;
+    String seats;
+    boolean aircon;
+    boolean cruiseControl;
+    int horsePower;
+
+    public Vehicle (String brand, String model, String fuelType, int registrationNumber,
+                    int firstRegistrationYearAndMonth, int odometer, boolean gear, String seats,
+                    boolean aircon, boolean cruiseControl, int horsePower)   {
+        this.brand = brand;
+        this.model = model;
+        this.fuelType = fuelType;
+        this.registrationNumber = registrationNumber;
+        this. firstRegistrationYearAndMonth = firstRegistrationYearAndMonth;
+        this.odometer = odometer;
+        this.gear = gear;
+        this.seats = seats;
+        this.aircon = aircon;
+        this.cruiseControl = cruiseControl;
+        this.horsePower = horsePower;
+
+    }
+    public Vehicle ()   {}
 }
